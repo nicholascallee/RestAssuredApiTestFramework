b1d54d11d95f9d599d4b07fb9cbef8abae41d59b
nicholascallee <nicholascallee@gmail.com>
2023-08-20 10:57:45 -0500

    added excel utility

---
+++ src/test/java/api/utilities/ExcelUtility.java
@@ -0,0 +1,65 @@
+package api.utilities;
+import org.apache.poi.ss.usermodel.CellStyle;
+import org.apache.poi.xssf.usermodel.XSSFCell;
+import org.apache.poi.xssf.usermodel.XSSFRow;
+import org.apache.poi.xssf.usermodel.XSSFSheet;
+import org.apache.poi.xssf.usermodel.XSSFWorkbook;
+
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.IOException;
+
+public class ExcelUtility {
+    public FileInputStream fi;
+    public FileOutputStream fo;
+    public XSSFWorkbook workbook;
+    public XSSFSheet sheet;
+    public XSSFRow row;
+    public XSSFCell cell;
+    public CellStyle style;
+    String path;
+    public ExcelUtility (String path) {
+        this.path = path;
+    }
+    public int getRowCount (String sheetName) throws IOException {
+        fi=new FileInputStream(path);
+        workbook=new XSSFWorkbook (fi);
+        sheet = workbook.getSheet (sheetName);
+        int rowcount=sheet.getLastRowNum();
+        workbook.close();
+        fi.close();
+        return rowcount;
+    }
+
+    //get cell count
+    public int getCellCount (String sheetName, int rownum) throws IOException {
+        fi=new FileInputStream(path);
+        workbook=new XSSFWorkbook (fi);
+        sheet = workbook.getSheet (sheetName);
+        row = sheet.getRow (rownum);
+        int cellcount=row.getLastCellNum();
+        workbook.close();
+        fi.close();
+        return cellcount;
+    }
+
+    //get cell data
+    public String getCellData (String sheetName, int rownum, int colnum) throws IOException {
+        fi=new FileInputStream(path);
+        workbook=new XSSFWorkbook (fi);
+        sheet = workbook.getSheet (sheetName);
+        row = sheet.getRow (rownum);
+        cell = row.getCell (colnum);
+        String data;
+        try {
+            data = cell.getStringCellValue();
+        } catch (Exception e) {
+            data = "";
+        }
+        workbook.close();
+        fi.close();
+        return data;
+    }
+
+}
