ef00e2d93c178e7bcf2445b04a40a6369501d33d
nicka <current-nicholascallee@gmail.com>
2023-08-19 20:45:39 -0500

    end point user methods

--- src/test/java/api/endpoints/Routes.java
+++ src/test/java/api/endpoints/Routes.java
@@ -5,6 +5,7 @@ public class Routes {
     public static final String BASE_URL = "https://petstore.swagger.io/v2";
     public static final String PET_URL = BASE_URL + "/pet";
     public static final String USER_URL = BASE_URL + "/user";
+    public static final String GET_USER_URL = USER_URL + "/{username}";
     public static final String STORE_URL = BASE_URL + "/store";
 
 }
--- src/test/java/api/endpoints/UserEndPoints.java
+++ src/test/java/api/endpoints/UserEndPoints.java
@@ -1,5 +1,41 @@
 package api.endpoints;
 
+import static io.restassured.RestAssured.given;
+import io.restassured.http.ContentType;
+import io.restassured.response.Response;
+
 public class UserEndPoints {
-    //58:30
+    public static Response createUser(User payload)
+    {
+        Response response = given()
+            .contentType(ContentType.JSON)
+            .accept(ContentType.JSON)
+            .body(payload)
+            .when()
+            .post(Routes.USER_URL);
+        return response;
+    }
+
+
+    public static Response readUser(String username)
+    {
+        Response response = given()
+                .pathParam("username", username)
+            .when()
+            .post(Routes.GET_USER_URL);
+        return response;
+    }
+
+    public static Response updateUser(String username, User payload)
+    {
+        Response response = given()
+            .contentType(ContentType.JSON)
+            .accept(ContentType.JSON)
+            .pathParam("username", username)
+            .body(payload)
+            .when()
+            .put(Routes.USER_URL);
+        return response;
+    }
+
 }
