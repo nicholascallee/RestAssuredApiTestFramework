60e2daba14e94230cee8758c485c7d6d09d3ecc8
nicka <current-nicholascallee@gmail.com>
2023-08-20 00:11:51 -0500

    more user tests created

--- src/test/java/api/test/UserTests.java
+++ src/test/java/api/test/UserTests.java
@@ -22,13 +22,42 @@ public class UserTests {
         userPayload.setEmail(faker.internet().emailAddress());
         userPayload.setPassword(faker.internet().password(8, 12));
         userPayload.setPhone(faker.phoneNumber().cellPhone());
-        userPayload.setUserStatus(1);
-
     }
 
     @Test(priority = 1)
     public void TestPostUser() {
         Response response = UserEndPoints.createUser(userPayload);
+        response.then().log().all();
+
+        Assert.assertEquals(response.statusCode(), 200);
+    }
+
+    @Test(priority = 2)
+    public void TestGetUser() {
+        Response response = UserEndPoints.readUser(userPayload.getUsername());
+        response.then().log().all();
+
+        Assert.assertEquals(response.statusCode(), 200);
+    }
+
+    @Test(priority = 3)
+    public void TestPutUser() {
+        //update user with new data
+        userPayload.setFirstName(faker.name().firstName());
+        userPayload.setLastName(faker.name().lastName());
+        userPayload.setEmail(faker.internet().emailAddress());
+        userPayload.setPassword(faker.internet().password(8, 12));
+        userPayload.setPhone(faker.phoneNumber().cellPhone());
+
+        Response response = UserEndPoints.updateUser(userPayload.getUsername(), userPayload);
+        response.then().log().all();
+
+        Assert.assertEquals(response.statusCode(), 200);
+    }
+
+    @Test(priority = 4)
+    public void TestDeleteUser() {
+        Response response = UserEndPoints.deleteUser(userPayload.getUsername());
         response.then().log().all();
 
         Assert.assertEquals(response.statusCode(), 200);
