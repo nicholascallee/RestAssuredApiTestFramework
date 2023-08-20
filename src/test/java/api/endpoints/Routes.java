de43e53c02ad319dc07a51894500666f0d34061e
nicka <current-nicholascallee@gmail.com>
2023-08-19 20:23:56 -0500

    routes created and endpoints class made

--- src/test/java/api/endpoints/Routes.java
+++ src/test/java/api/endpoints/Routes.java
@@ -2,5 +2,9 @@ package api.endpoints;
 
 
 public class Routes {
-    //40:56
+    public static final String BASE_URL = "https://petstore.swagger.io/v2";
+    public static final String PET_URL = BASE_URL + "/pet";
+    public static final String USER_URL = BASE_URL + "/user";
+    public static final String STORE_URL = BASE_URL + "/store";
+
 }
