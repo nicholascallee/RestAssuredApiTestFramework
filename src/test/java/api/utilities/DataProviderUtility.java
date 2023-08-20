846b34cc5e79a0226d8b4f1bf52d2a2c13bd0f7e
nicholascallee <nicholascallee@gmail.com>
2023-08-20 17:01:09 -0500

    get usernames data provider

--- src/test/java/api/utilities/DataProviderUtility.java
+++ src/test/java/api/utilities/DataProviderUtility.java
@@ -5,18 +5,30 @@ import org.testng.annotations.DataProvider;
 public class DataProviderUtility {
 
     @DataProvider(name = "Data")
-    public String[][] getData() throws Exception {
-        String path = System.getProperty("user.dir") + "/testdata/TestData.xlsx";
+    public String[][] getData(String dataFileName, String sheetName) throws Exception {
+        String path = System.getProperty("user.dir") + "/testdata/" + dataFileName + ".xlsx";
         ExcelUtility excel = new ExcelUtility(path);
-        int rowNum = excel.getRowCount("Sheet1");
-        int colCount = excel.getCellCount("Sheet1", 1);
+        int rowNum = excel.getRowCount(sheetName);
+        int colCount = excel.getCellCount(sheetName, 1);
         String data[][] = new String[rowNum][colCount];
         for (int i = 1; i <= rowNum; i++) {
             for (int j = 0; j < colCount; j++) {
-                data[i - 1][j] = excel.getCellData("Sheet1", i, j);
+                data[i - 1][j] = excel.getCellData(sheetName, i, j);
             }
         }
         return data;
+    }
+
+    @DataProvider(name = "Usernames")
+    public String[] getUsernames(String dataFileName, String sheetName) throws Exception {
+        String path = System.getProperty("user.dir") + "/testdata/" + dataFileName + ".xlsx";
+        ExcelUtility excel = new ExcelUtility(path);
+        int rowNum = excel.getRowCount(sheetName);
+        String usernameData[] = new String[rowNum];
+        for (int i = 1; i <= rowNum; i++) {
+            usernameData[i - 1] = excel.getCellData(sheetName, i, 1);
+        }
+        return usernameData;
     }
 
 
