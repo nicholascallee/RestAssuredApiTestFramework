46b24872a616c1d3f9fdaf383e7a38659563f787
nicholascallee <nicholascallee@gmail.com>
2023-08-20 17:33:44 -0500

    Data Driven Tests.

---
+++ src/test/java/api/test/DataDrivenTests.java
@@ -0,0 +1,37 @@
+package api.test;
+
+import api.endpoints.UserEndPoints;
+import api.payload.User;
+import api.utilities.DataProviderUtility;
+import io.restassured.response.Response;
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+public class DataDrivenTests {
+
+    @Test(priority = 1, dataProvider = "Data", dataProviderClass = DataProviderUtility.class)
+    public void testPostUser(String id, String username, String firstName, String lastName, String email, String password, String phone) {
+        //TODO: fix the id here so it actually works
+        User userPayload = new User();
+        Integer intId = null;
+        try{
+            intId = Integer.parseInt(id);
+        }
+        catch (NumberFormatException e){
+            System.out.println("Error converting id to integer");
+            intId = 0;
+        }
+
+        userPayload.setId(intId);
+        userPayload.setUsername(username);
+        userPayload.setFirstName(firstName);
+        userPayload.setLastName(lastName);
+        userPayload.setEmail(email);
+        userPayload.setPassword(password);
+        userPayload.setPhone(phone);
+
+        Response response = UserEndPoints.createUser(userPayload);
+        Assert.assertEquals(response.statusCode(), 200);
+
+    }
+}
